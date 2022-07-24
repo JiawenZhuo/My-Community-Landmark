f00b628572224fdc184a017503b9b5164b910706
JiawenZhuo <jiawen.zhuo@uq.net.au>
2022-07-24 11:58:30 +1000

    add note by location

---
+++ server/router/landmark-router.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+
+const landmarkController = require('../controller/landmark-controller');
+var bodyParser = require('body-parser')
+ 
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const router = express.Router()
+
+router.post('/landmark',jsonParser, landmarkController.createLandmark)
+router.put('/update', jsonParser,landmarkController.updateLandmark)
+// router.delete('/movie/:id', MovieCtrl.deleteMovie)
+// router.get('/movie/:id', MovieCtrl.getMovieById)
+router.get('/get', jsonParser, landmarkController.getLandmarks)
+
+module.exports = router;
